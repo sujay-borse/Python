79e2d4ba576a52d4c38761537c4e22675d459f43
Sujay Borse <sujayborse09@gmail.com>
2026-01-20 15:56:12 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,11 @@
+num=int(input("Enter three digit number="))
+sum=0
+temp=num
+while temp>0:
+    digit=temp%10
+    sum+=digit**3
+    temp=temp/10
+if num==sum:
+    print("It is an armstrong number")
+else:
+    print("It is not an armstrong number")
