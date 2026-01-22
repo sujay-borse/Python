84de7e6587f38f9d2b6c54a855bf7a2a22d414ef
Sujay Borse <sujayborse09@gmail.com>
2026-01-22 14:18:21 +0530

    Add files via upload

---
+++ b.py
@@ -0,0 +1,11 @@
+# rows=int(input("Enter rows="))
+# i=0
+# while i<rows:
+#     print(" " * (rows-i-1)+"*"*(2*i+1))
+#     i=i+1
+
+rows=int(input("Enter rows="))
+i=0
+while i<rows:
+    print(" " * (rows-i-1) + "*" * (2*i+1))
+    i=i+1
