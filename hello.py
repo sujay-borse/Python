e06ec67e6522a08ac8408efa55d913506b9e7df9
Sujay Borse <sujayborse09@gmail.com>
2026-02-01 18:37:56 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number:-"))
+b=int(input("Enter second number:-"))
+print("Before swapping values are:-")
+print(a,b)
+temp=a
+a=b
+b=temp
+print("After swpping values are:-")
+print(a,b)
