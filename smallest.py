9162bc971ab844eb53d48218e0fb8b775192e198
Sujay Borse <sujayborse09@gmail.com>
2026-02-22 16:36:46 +0530

    Add files via upload

---
+++ smallest.py
@@ -0,0 +1,12 @@
+a=int(input("Enter first number:-"))
+b=int(input("Enter second number:-"))
+c=int(input("Enter third number:-"))
+if a<b:
+elif a<c:
+    print(a,"is a smallest number")
+else:
+    print(c,"is a smallest number")
+if b<c:
+    print(b,"is a smallest number")
+else:
+    print(c,"is a smallest number")
